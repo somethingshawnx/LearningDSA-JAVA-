a57acb3be833a838a3f1b41b4ac7c29c88a71a35
somethingshawnx <127673207+somethingshawnx@users.noreply.github.com>
2025-05-17 15:28:54 +0530

    Add files via upload
    find the numbers have even digits

---
+++ Solution.java
@@ -0,0 +1,31 @@
+//https://leetcode.com/problems/find-numbers-with-even-number-of-digits/
+
+class Solution {
+    public int findNumbers(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (even(num)) {
+                count++;
+            }
+        }
+        return count;    
+    }
+
+    static boolean even(int num) {
+        int numberOfDigits = digits(num);
+        return numberOfDigits % 2 == 0;
+    }
+
+    static int digits(int num) {
+        int count = 0;
+        
+        
+        
+        while (num > 0) {
+            count++;
+            num = num / 10;
+        }
+        
+        return count;
+    }
+}
